4fd3bf013a63b2dabf8799975e8a7676ebf26273
730004251 <dawidwas@macbook-pro-dawid.local>
2019-01-16 12:08:53 +0100

    Initial Commit

---
+++ Kantor/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Kantor
+//
+//  Created by Dawid Was on 16/01/2019.
+//  Copyright © 2019 Dawid Was. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Kantor/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Kantor
+//
+//  Created by Dawid Was on 16/01/2019.
+//  Copyright © 2019 Dawid Was. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
